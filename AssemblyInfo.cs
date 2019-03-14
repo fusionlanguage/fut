77d78ca41896571ae4196feeb05c2020784a29c1
Piotr Fusik <fox@scene.pl>
2019-03-14 05:23:54 +0100

    [doc] Bump year.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Foxoft")]
 [assembly: AssemblyProduct("Ci Programming Language")]
-[assembly: AssemblyCopyright("Copyright © Foxoft 2011-2018")]
+[assembly: AssemblyCopyright("Copyright © Foxoft 2011-2019")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
