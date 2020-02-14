4abc4a48a46c43cbcdd8472e19d0dee3b3a3e2e3
Piotr Fusik <fox@scene.pl>
2020-02-14 09:22:43 +0100

    [cleanup] Unused variable.

--- GenJs.cs
+++ GenJs.cs
@@ -310,7 +310,7 @@ public class GenJs : GenBase
 			}
 			Write(')');
 		}
-		else if (obj.Type is CiArrayType array && method.Name == "CopyTo") {
+		else if (obj.Type is CiArrayType && method.Name == "CopyTo") {
 			AddLibrary(GenJsMethod.CopyArray,
 				"copyArray : function(sa, soffset, da, doffset, length)",
 				"if (typeof(sa.subarray) == \"function\" && typeof(da.set) == \"function\")",
