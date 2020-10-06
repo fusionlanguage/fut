e5ab335a2d49a97148995cede3444e9d7bd0d121
Andy Edwards <jedwards@fastmail.com>
2020-10-05 23:57:43 -0500

    fix(CiTo): revert accidental formatting changes

--- CiTo.cs
+++ CiTo.cs
@@ -28,139 +28,129 @@ using System.Reflection;
 namespace Foxoft.Ci
 {
 
-  public static class CiTo
-  {
-    static void Usage()
-    {
-      Console.WriteLine("Usage: cito [OPTIONS] -o FILE INPUT.ci");
-      Console.WriteLine("Options:");
-      Console.WriteLine("-l c       Translate to C");
-      Console.WriteLine("-l cpp     Translate to C++");
-      Console.WriteLine("-l cs      Translate to C#");
-      Console.WriteLine("-l java    Translate to Java");
-      Console.WriteLine("-l js      Translate to JavaScript");
-      Console.WriteLine("-l py      Translate to Python");
-      Console.WriteLine("-l swift   Translate to Swift");
-      Console.WriteLine("-l cl      Translate to OpenCL C");
-      Console.WriteLine("-l d.ts    Translate to TypeScript declarations");
-      Console.WriteLine("-o FILE    Write to the specified file");
-      Console.WriteLine("-n NAME    Specify C++/C# namespace, Java package or C name prefix");
-      Console.WriteLine("-D NAME    Define conditional compilation symbol");
-      Console.WriteLine("-r FILE.ci Read the specified source file but don't emit code");
-      Console.WriteLine("-I DIR     Add directory to resource search path");
-      Console.WriteLine("--help     Display this information");
-      Console.WriteLine("--version  Display version information");
-    }
+public static class CiTo
+{
+	static void Usage()
+	{
+		Console.WriteLine("Usage: cito [OPTIONS] -o FILE INPUT.ci");
+		Console.WriteLine("Options:");
+		Console.WriteLine("-l c       Translate to C");
+		Console.WriteLine("-l cpp     Translate to C++");
+		Console.WriteLine("-l cs      Translate to C#");
+		Console.WriteLine("-l java    Translate to Java");
+		Console.WriteLine("-l js      Translate to JavaScript");
+		Console.WriteLine("-l py      Translate to Python");
+		Console.WriteLine("-l swift   Translate to Swift");
+		Console.WriteLine("-l cl      Translate to OpenCL C");
+		Console.WriteLine("-l d.ts    Translate to TypeScript declarations");
+		Console.WriteLine("-o FILE    Write to the specified file");
+		Console.WriteLine("-n NAME    Specify C++/C# namespace, Java package or C name prefix");
+		Console.WriteLine("-D NAME    Define conditional compilation symbol");
+		Console.WriteLine("-r FILE.ci Read the specified source file but don't emit code");
+		Console.WriteLine("-I DIR     Add directory to resource search path");
+		Console.WriteLine("--help     Display this information");
+		Console.WriteLine("--version  Display version information");
+	}
 
-    static CiProgram ParseAndResolve(CiParser parser, CiScope parent, List<string> files, List<string> searchDirs)
-    {
-      parser.Program = new CiProgram { Parent = parent };
-      foreach (string file in files)
-        parser.Parse(file, File.OpenText(file));
-      new CiResolver(parser.Program, searchDirs);
-      return parser.Program;
-    }
+	static CiProgram ParseAndResolve(CiParser parser, CiScope parent, List<string> files, List<string> searchDirs)
+	{
+		parser.Program = new CiProgram { Parent = parent };
+		foreach (string file in files)
+			parser.Parse(file, File.OpenText(file));
+		new CiResolver(parser.Program, searchDirs);
+		return parser.Program;
+	}
 
-    public static int Main(string[] args)
-    {
-      CiParser parser = new CiParser();
-      List<string> inputFiles = new List<string>();
-      List<string> referencedFiles = new List<string>();
-      List<string> searchDirs = new List<string>();
-      string lang = null;
-      string outputFile = null;
-      string namespace_ = null;
-      for (int i = 0; i < args.Length; i++)
-      {
-        string arg = args[i];
-        if (arg[0] == '-')
-        {
-          switch (arg)
-          {
-            case "--help":
-              Usage();
-              return 0;
-            case "--version":
-              Console.WriteLine("cito 1.0.0");
-              return 0;
-            case "-l":
-              lang = args[++i];
-              break;
-            case "-o":
-              outputFile = args[++i];
-              break;
-            case "-n":
-              namespace_ = args[++i];
-              break;
-            case "-D":
-              string symbol = args[++i];
-              if (symbol == "true" || symbol == "false")
-                throw new ArgumentException(symbol + " is reserved");
-              parser.PreSymbols.Add(symbol);
-              break;
-            case "-r":
-              referencedFiles.Add(args[++i]);
-              break;
-            case "-I":
-              searchDirs.Add(args[++i]);
-              break;
-            default:
-              throw new ArgumentException("Unknown option: " + arg);
-          }
-        }
-        else
-        {
-          inputFiles.Add(arg);
-        }
-      }
-      if (lang == null && outputFile != null)
-      {
-        string ext = Path.GetExtension(outputFile);
-        if (ext.Equals(".ts") && outputFile.EndsWith(".d.ts"))
+	public static int Main(string[] args)
+	{
+		CiParser parser = new CiParser();
+		List<string> inputFiles = new List<string>();
+		List<string> referencedFiles = new List<string>();
+		List<string> searchDirs = new List<string>();
+		string lang = null;
+		string outputFile = null;
+		string namespace_ = null;
+		for (int i = 0; i < args.Length; i++) {
+			string arg = args[i];
+			if (arg[0] == '-') {
+				switch (arg) {
+				case "--help":
+					Usage();
+					return 0;
+				case "--version":
+					Console.WriteLine("cito 1.0.0");
+					return 0;
+				case "-l":
+					lang = args[++i];
+					break;
+				case "-o":
+					outputFile = args[++i];
+					break;
+				case "-n":
+					namespace_ = args[++i];
+					break;
+				case "-D":
+					string symbol = args[++i];
+					if (symbol == "true" || symbol == "false")
+						throw new ArgumentException(symbol + " is reserved");
+					parser.PreSymbols.Add(symbol);
+					break;
+				case "-r":
+					referencedFiles.Add(args[++i]);
+					break;
+				case "-I":
+					searchDirs.Add(args[++i]);
+					break;
+				default:
+					throw new ArgumentException("Unknown option: " + arg);
+				}
+			}
+			else {
+				inputFiles.Add(arg);
+			}
+		}
+		if (lang == null && outputFile != null) {
+			string ext = Path.GetExtension(outputFile);
+			if (ext.Equals(".ts") && outputFile.EndsWith(".d.ts"))
           ext = ".d.ts";
-        if (ext.Length >= 2)
-          lang = ext.Substring(1);
-      }
-      if (lang == null || outputFile == null || inputFiles.Count == 0)
-      {
-        Usage();
-        return 1;
-      }
-      GenBase gen;
-      switch (lang)
-      {
-        case "c": gen = new GenC(); break;
-        case "cpp": gen = new GenCpp(); break;
-        case "cs": gen = new GenCs(); break;
-        case "java": gen = new GenJava(); break;
-        case "js": gen = new GenJs(); break;
-        case "py": gen = new GenPy(); break;
-        case "swift": gen = new GenSwift(); break;
-        case "cl": gen = new GenCl(); break;
-        case "d.ts": gen = new GenTs(); break;
-        default: throw new ArgumentException("Unknown language: " + lang);
-      }
-      gen.Namespace = namespace_;
-      gen.OutputFile = outputFile;
+			if (ext.Length >= 2)
+				lang = ext.Substring(1);
+		}
+		if (lang == null || outputFile == null || inputFiles.Count == 0) {
+			Usage();
+			return 1;
+		}
+		GenBase gen;
+		switch (lang) {
+		case "c": gen = new GenC(); break;
+		case "cpp": gen = new GenCpp(); break;
+		case "cs": gen = new GenCs(); break;
+		case "java": gen = new GenJava(); break;
+		case "js": gen = new GenJs(); break;
+		case "py": gen = new GenPy(); break;
+		case "swift": gen = new GenSwift(); break;
+		case "cl": gen = new GenCl(); break;
+		case "d.ts": gen = new GenTs(); break;
+		default: throw new ArgumentException("Unknown language: " + lang);
+		}
+		gen.Namespace = namespace_;
+		gen.OutputFile = outputFile;
 
-      CiProgram program;
-      try
-      {
-        CiScope parent = CiSystem.Value;
-        if (referencedFiles.Count > 0)
-          parent = ParseAndResolve(parser, parent, referencedFiles, searchDirs);
-        program = ParseAndResolve(parser, parent, inputFiles, searchDirs);
-      }
-      catch (CiException ex)
-      {
-        Console.Error.WriteLine("{0}({1}): ERROR: {2}", ex.Filename, ex.Line, ex.Message);
-        return 1;
-        //			throw;
-      }
+		CiProgram program;
+		try {
+			CiScope parent = CiSystem.Value;
+			if (referencedFiles.Count > 0)
+				parent = ParseAndResolve(parser, parent, referencedFiles, searchDirs);
+			program = ParseAndResolve(parser, parent, inputFiles, searchDirs);
+		} catch (CiException ex) {
+			Console.Error.WriteLine("{0}({1}): ERROR: {2}", ex.Filename, ex.Line, ex.Message);
+			return 1;
+//			throw;
+		}
 
-      gen.Write(program);
-      return 0;
-    }
-  }
+		gen.Write(program);
+		return 0;
+	}
+}
 
 }
