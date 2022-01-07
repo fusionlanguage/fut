6c0f851f75eca968c2aa5ff5fc712a5b3a29fed0
Piotr Fusik <fox@scene.pl>
2022-01-07 10:45:51 +0100

    [doc] Bump the copyright year.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Foxoft")]
 [assembly: AssemblyProduct("Ci Programming Language")]
-[assembly: AssemblyCopyright("Copyright © Piotr Fusik 2011-2021")]
+[assembly: AssemblyCopyright("Copyright © Piotr Fusik 2011-2022")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
