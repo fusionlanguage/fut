d0974aaa5d18980ab7422536f343ab448fcbb3fa
Piotr Fusik <fox@scene.pl>
2023-05-25 19:07:03 +0200

    Revert "[test] "cito" is a superset of "cicheck"."
    This reverts commit fc10f762daa6e0f35235cee7ee5bb41404f98473.

---
+++ test/CiCheck.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "Test.hpp"
+
+int main(int argc, char **argv)
+{
+	std::shared_ptr<CiSystem> system = CiSystem::new_();
+	CiProgram program;
+	program.parent = system.get();
+	program.system = system.get();
+	CiConsoleParser parser;
+	parser.program = &program;
+	std::string input;
+	for (int i = 1; i < argc; i++) {
+		const char *inputFilename = argv[i];
+		std::getline(std::ifstream(inputFilename), input, '\0');
+		parser.parse(inputFilename, reinterpret_cast<const uint8_t *>(input.data()), input.size());
+	}
+	CiSema sema;
+	sema.process(&program);
+	std::cout << "PASSED\n";
+}
