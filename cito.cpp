722bf1f00b8e20da061543ff9c135f057c1ce650
Piotr Fusik <piotr@fusion-lang.org>
2023-08-03 10:21:00 +0200

    [brand] Copyright headers.

--- cito.cpp
+++ cito.cpp
@@ -1,21 +1,22 @@
-// cito.cpp - Ci transpiler
+// cito.cpp - Fusion transpiler
 //
 // Copyright (C) 2011-2023  Piotr Fusik
 //
-// This file is part of CiTo, see https://github.com/pfusik/cito
+// This file is part of Fusion Transpiler,
+// see https://github.com/fusionlanguage/fut
 //
-// CiTo is free software: you can redistribute it and/or modify
+// Fusion Transpiler is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// CiTo is distributed in the hope that it will be useful,
+// Fusion Transpiler is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with CiTo.  If not, see http://www.gnu.org/licenses/
+// along with Fusion Transpiler.  If not, see http://www.gnu.org/licenses/
 
 #include <cstring>
 #include <filesystem>
