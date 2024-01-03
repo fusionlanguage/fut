4d8e345338c965165ec8904d93e1e7411465905e
Piotr Fusik <piotr@fusion-lang.org>
2024-01-03 16:23:30 +0100

    [doc] Bump the copyright year.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Fusion Programming Language")]
-[assembly: AssemblyCopyright("Copyright © Piotr Fusik 2011-2023")]
+[assembly: AssemblyCopyright("Copyright © Piotr Fusik 2011-2024")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("3.1.1.0")]
 [assembly: AssemblyFileVersion("3.1.1.0")]
